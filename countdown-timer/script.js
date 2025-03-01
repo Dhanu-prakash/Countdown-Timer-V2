13472017ad419ad468b186bea67deea0c4e091fa
Dhanu-prakash <dhanuprakash.edu@gmail.com>
2025-03-01 11:55:51 +0530

    Update script.js

--- countdown-timer/script.js
+++ countdown-timer/script.js
@@ -5,6 +5,20 @@ const Seconds = document.getElementById('seconds');
 
 const targetDate = new Date("November 1 2025 00:00:00").getTime();
 
+document.addEventListener("DOMContentLoaded", function() {
+    const bgMusic = document.getElementById("bgMusic");
+    bgMusic.volume = 0.2;
+
+    document.addEventListener("click", function() {
+
+        
+        if (bgMusic.paused) {
+            bgMusic.play();
+        }
+    });
+});
+
+
 function timer () {
     const currentDate = new Date().getTime();
     const distance = targetDate - currentDate;
@@ -13,8 +27,6 @@ function timer () {
     const hours = Math.floor(distance / 1000 / 60 / 60) % 24;
     const minutes = Math.floor(distance / 1000 / 60) % 60;
     const seconds = Math.floor(distance / 1000) % 60;
-
-    console.log(days + ":" + hours + ":" + minutes + ":" + seconds);
 
     Days.innerHTML = days;
     Hours.innerHTML = hours;
